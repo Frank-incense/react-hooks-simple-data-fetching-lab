96282775d5deef70c34a7df33b9f333ffa70a24a
Frankincense <frankincensewelsey@gmail.com>
2025-04-14 20:28:06 +0300

    Fetch lab done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import { useEffect, useState } from "react";
+const API = "https://dog.ceo/api/breeds/image/random";
+
+function App(){
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [image, setImage] = useState(null)
+
+    useEffect(()=>{
+        fetch(API)
+        .then(r => r.json())
+        .then(data => {
+            console.log(data)
+            setIsLoaded(isLoaded => !isLoaded)
+            setImage(data.message)
+        })
+    },[])
+
+    return(
+    <>
+        {
+            !isLoaded
+            ? <p>Loading...</p>
+            : <img src={image} alt="A Random Dog"/>
+        }
+    </>)   
+}
+
+export default App;
